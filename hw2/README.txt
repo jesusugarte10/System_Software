a335e8e784712944cbcd4ddd8995eedf64430d8e
Jesus Ugarte <Jugarteosta@knights.ucf.edu>
2021-02-23 17:48:30 -0500

    Final Project Done

--- hw2/README.txt
+++ hw2/README.txt
@@ -1,8 +1,10 @@
-Authors:
+Author:
 Jesus Ugarte
-Mina Beshay
 
 
 To use the program please enter filename in the first argument when running the program in terminal.
 
 Also make sure the infile is in the same directory
+tester can be used if run int he terminal 
+
+bash tester.sh
--- hw2/lex.c
+++ hw2/lex.c
@@ -1,9 +1,9 @@
-/* University of Central Florida
+/* 
+University of Central Florida
 Lexical Analyzer/Scanner
-
-Members:
+COP 3402 System Software: HW1
+Member:
 Jesus Ugarte
-Mina Beshay
  */
 
 #include <stdio.h>
@@ -11,53 +11,422 @@ Mina Beshay
 #include <stdlib.h>
 #include <ctype.h>
 
-//WE ARE WORKING HERE!!
-
-
-//FROM ABENDIX B In assignment desciption
-//Declaration of Token Types:
+// Modified.
 typedef enum { 
-modsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6,  slashsym = 7, oddsym = 8, eqsym = 9, neqsym = 10, lessym = 11, leqsym = 12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17, semicolonsym = 18, periodsym = 19, becomessym = 20, beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31, readsym = 32, elsesym = 33, returnsym = 34} token_type;
+    modsym = 1, identsym, numbersym, plussym, minussym, multsym, slashsym,
+    oddsym, eqlsym, neqsym, lessym, leqsym, gtrsym, geqsym, lparentsym,
+    rparentsym, commasym, semicolonsym, periodsym, becomessym,
+    beginsym, endsym, ifsym, thensym, whilesym, dosym,callsym,
+    constsym, varsym, procsym, writesym, readsym, elsesym, returnsym
+} token_type;
 
-
-//For Reserved Special Symbols
-const char specialSymbols[]={'+', '-', '*', '/', '(', ')', '=', ',' , '.', '<', '>',  ';' , ':', '%'};
-
-//For Reserved Words || We are not using 'begin' and 'end' as stated in doc <- may need to use it? 
-const char* reservedWords[]={"const", "var", "procedure", "call", "if", "then", "else", "while", "do", "read", "write", "odd"};
-
-
-//struct used to contain all tokens details
 typedef struct {
-    token_type tokenID;
-    int numberValue;
-    char name[12];
-}tokenStruct;
+  token_type token;
+  int number_v;
+  char name[12];
+}token_struct;
 
-tokenStruct lexList[5000];
-int lexListIndex = 0;
+token_struct lex_list[5000];
+int lex_index;
 
-int main(int argc, char **argv){
+// For Reserved Words given to us (Modified)
+const char* reserved_words[]={"const", "var", "procedure", "call", "begin", "end", "if", 
+"then", "else", "while", "do", "read", "write", "odd", "return"};
 
-  //FILE Management Section
-  FILE* ifp;
-  ifp = fopen(argv[1], "r");
-  if (!ifp)
-  {
-    printf("can't read file");
-    return 0;
-  }
+// Special Symbols given to us. (Modified)
+const char special_symbols[]={'+', '-', '*', '/', '(', ')', '=', ',' , '.', '<', '>',  ';' , ':', '%'};
 
-  int c = fgetc(ifp);
-  //Ignores spaces, tabs, and newlines aka whitespace
-  while(c != EOF)
-  {
-    c = fgetc(ifp);
-    /*
-    Main Program goes HERE
-    */
-  }
 
-  fclose(ifp);
+token_struct lex_list[5000];
+int lex_index = 0;
+
+
+int main(int argc, char *argv[]){
+
+    int counter;
+    for(counter = 0; counter < 5000; counter++){
+      lex_list[counter].token = 0;
+    }
+    
+    // File pointer to open our input.
+    FILE *ifp = fopen(argv[1], "r"); 
+
+    // Variable declarations
+    int i = 0; 
+    int j = 0; 
+    int k = 0;
+
+    // Errors variable to store them
+    int errors;
+    // Variable to hold each character read in
+    int character;
+    //For comments
+    int comments = 0;
+    //Looks ahead at next character read in
+    int lookAhead=0;
+
+    printf("Lexeme Table:\n");
+    printf("\tlexeme\ttoken type\n");
+
+    character = fgetc(ifp);
+    //Ignores spaces, tabs, and newlines aka whitespace
+    while(character != EOF){
+        if((iscntrl(character) != 0) || isspace(character)){
+            character = fgetc(ifp);
+            lookAhead = 0;
+            continue;
+        }
+        //reads if the next character is part of the alphabet
+        if(isalpha(character)){
+            char string[12];
+            memset(string, 0, sizeof string);
+            
+            int index = 0;
+            string[index]=character;
+
+            index++;
+            lookAhead=1;
+            //Prints Error 3 if the variable name is too long
+            while(isalpha(character=fgetc(ifp))||isdigit(character)){
+                if(index > 10){
+                    printf("Error : Identifier names cannot exceed 11 characters\n");
+
+                    //Error Checking
+                    while (isalpha(character=fgetc(ifp))||isdigit(character)) {
+
+                    }
+                    errors=1;
+
+                    //lookAhead=0;
+                    break;
+                }
+                string[index]=character;
+                index++;
+            }
+
+            //If there was an error, continue without accepting token
+            if(errors == 1) {
+                errors = 0;
+            }
+
+            //Compares the variable name to see if it is one of the reserved words
+            int reservedSwitch=-1;
+
+            for(i=0; i<14;i++){
+                if(strcmp(string, reserved_words[i])==0){
+                    reservedSwitch=i;
+                }
+            }
+
+            //If it is a reserved word, print out the correct tokentype
+            switch(reservedSwitch){
+
+                //Case for const
+                case 0:
+                    lex_list[lex_index].token = constsym;
+                    break;
+                //Case for var
+                case 1:
+                    lex_list[lex_index].token = varsym;
+                    break;
+                //Case for procedure
+                case 2:
+                    lex_list[lex_index].token = procsym;
+                    break;
+                //Case for call
+                case 3:
+                    lex_list[lex_index].token = callsym;
+                    break;
+                //Case for begin
+                case 4:
+                    lex_list[lex_index].token = beginsym;
+                    break;
+                //Case for end
+                case 5:
+                    lex_list[lex_index].token = endsym;
+                    break;
+                //Case for if
+                case 6:
+                    lex_list[lex_index].token = ifsym;
+                    break;
+                //Case for then
+                case 7:
+                    lex_list[lex_index].token = thensym;
+                    break;
+                //Case for else
+                case 8:
+                    lex_list[lex_index].token = elsesym;
+                    break;
+                //Case for while
+                case 9:
+                    lex_list[lex_index].token = whilesym;
+                    break;
+                //Case for do
+                case 10:
+                    lex_list[lex_index].token = dosym;
+                    break;
+                //Case for read
+                case 11:
+                    lex_list[lex_index].token = readsym;
+                    break;
+                //Case for write
+                case 12:
+                    lex_list[lex_index].token = writesym;
+                    break;
+                //Case for odd
+                case 13:
+                    lex_list[lex_index].token = oddsym;
+                    break;
+                //Case for return
+                case 14:
+                    lex_list[lex_index].token = returnsym;
+                    break;
+                //default case when it is a symbol
+                default:
+                    if(strlen(string) < 10){
+                        lex_list[lex_index].token = identsym;
+                        strcpy(lex_list[lex_index].name,string);
+                    }
+                    break;
+            }
+            
+            //Making sure only the right value is printed
+            if(strlen(string) < 10){
+                printf("\t%s\t%d\n", string, lex_list[lex_index].token);
+                lex_index++;
+            }
+        }
+        //reads if the next character is part of the 0-9 digits
+        else if(isdigit(character)){
+            int number = character - '0';
+            int d;
+            int place = 1;
+
+            lookAhead = 1;
+
+            //Prints Error 2 if the number is too long
+            while(isdigit(character = fgetc(ifp))){
+                if(place > 4){
+                    printf("Error : Numbers cannot exceed 5 digits\n");
+
+                    //Error checking
+                    while (isdigit(character = fgetc(ifp))) {
+
+                    }
+                    errors = 1;
+                    //lookAhead=0;
+                    break;
+                }
+                d=character - '0';
+                number=10*number+d;
+                place++;
+            }
+            //Prints Error 1 if the variable starts with a digit, and not a number
+            if(isalpha(character)){
+                
+                //handle error when identifiers cannot begin witha  digit
+                if(errors != 1)
+                    printf("Error : Identifiers cannot begin with a digit\n");
+
+                while(isalpha(character = fgetc(ifp)) || isdigit(character)){
+                }
+                continue;
+             }
+
+            //If there was an error, continue without accepting token
+            if(errors == 1) {
+                errors = 0;
+                continue;
+            }
+
+            lex_list[lex_index].token = numbersym;
+            lex_list[lex_index].number_v = number;
+
+            printf("\t%d\t%d\n", lex_list[lex_index].number_v, lex_list[lex_index].token);
+            lex_index++;
+        }
+
+        //reads if the next character is part of the special symbols
+        else {
+            lookAhead = 0;
+            int spec = -1;
+            for(i = 0; i < 14; i++){
+                if(character == special_symbols[i]){
+                    spec = i;
+                }
+            }
+            //If it is a special symbol, print out the correct tokentype
+            switch(spec){
+                //Case for +
+                case 0:
+                    lex_list[lex_index].token = plussym;
+                    lex_index++;
+                    break;
+                //Case for -
+                case 1:
+                    lex_list[lex_index].token = minussym;
+                    lex_index++;
+                    break;
+                //Case for *
+                case 2:
+                    lex_list[lex_index].token = multsym;
+                    lex_index++;
+                    break;
+                //Case for comments
+                case 3:
+                    character = fgetc(ifp);
+                    lookAhead=1;
+                    if(character == '*'){
+                        comments = 1;
+                        lookAhead = 0;
+                        character = fgetc(ifp);
+                        while(comments == 1){
+                            if(character == '*'){
+                                character = fgetc(ifp);
+                                if(character == '/'){
+                                    comments = 0;
+                                }
+                            }
+                            else{
+                                character = fgetc(ifp);
+                            }
+                        }
+                    }
+                    else{
+                        lex_list[lex_index].token = slashsym;
+                        lex_index++;
+                    }
+                    break;
+                //Case for (
+                case 4:
+                    lex_list[lex_index].token = lparentsym;
+                    lex_index++;
+                    break;
+                //Case for )
+                case 5:
+                    lex_list[lex_index].token = rparentsym;
+                    lex_index++;
+                    break;
+                //Case for =
+                case 6:
+                    lex_list[lex_index].token = eqlsym;
+                    lex_index++;
+                    break;
+                //Case for ,
+                case 7:
+                    lex_list[lex_index].token = commasym;
+                    lex_index++;
+                    break;
+                //Case for .
+                case 8:
+                    lex_list[lex_index].token = periodsym;
+                    lex_index++;
+                    break;
+                //Case for <>
+                case 9:
+                    character = fgetc(ifp);
+                    lookAhead=1;
+                    if(character == '>'){
+                        lex_list[lex_index].token = neqsym;
+                        lookAhead=0;
+                    }
+                    //Case for <=
+                    else if(character == '='){
+                        lex_list[lex_index].token = leqsym;
+                        lookAhead=0;
+                    }
+                    //Case for <
+                    else{
+                        lex_list[lex_index].token = lessym;
+                    }
+                    lex_index++;
+                    break;
+                //Case for >=
+                case 10:
+                    character = fgetc(ifp);
+                    lookAhead = 1;
+                    if(character == '='){
+                        lex_list[lex_index].token = geqsym;
+                        lookAhead = 0;
+                    }
+                    //Case for >
+                    else{
+                        lex_list[lex_index].token = gtrsym;
+                    }
+                    lex_index++;
+                    break;
+                //Case for ;
+                case 11:
+                    lex_list[lex_index].token = semicolonsym;
+                    lex_index++;
+                    break;
+                //Case for :=
+                case 12:
+                    character = fgetc(ifp);
+                    if(character == '='){
+                        lex_list[lex_index].token = becomessym;
+                        lex_index++;
+                    }
+                    //Prints Error 4 for invalid symbols
+                    else{
+                        printf("Error : Invalid Symbol\n");
+                    }
+                    break;
+                case 13:
+                    lex_list[lex_index].token = modsym;
+                    lex_index++;
+                    break;
+
+                //Prints Error 4 for invalid symbols
+                default:
+                    printf("Error : Invalid Symbol\n");
+                    break;
+            }
+            
+            //Making sure only to print when there is a valid symbol
+            if((spec>=0 ) && (spec<=13))
+            {    //Handling cases where char is double digit based on token value
+                if(lex_list[lex_index - 1].token == becomessym)
+                    printf("\t%s\t%d\n", ":=", lex_list[lex_index - 1].token);
+                else if(lex_list[lex_index - 1].token == neqsym)
+                    printf("\t%s\t%d\n", "<>", lex_list[lex_index - 1].token);
+                else if(lex_list[lex_index - 1].token == leqsym)
+                    printf("\t%s\t%d\n", "<=", lex_list[lex_index - 1].token);
+                else if(lex_list[lex_index - 1].token == geqsym)
+                    printf("\t%s\t%d\n", ">=", lex_list[lex_index - 1].token);
+                else if((spec == 3) && (comments==0)){
+                    printf("");
+                    comments = 1 ;
+                }
+                else
+                    printf("\t%c\t%d\n", special_symbols[spec], lex_list[lex_index - 1].token);
+            }
+        }
+        //if we aren't looking at next character, read in next as a part of a new variable/number/symbol
+        if(lookAhead == 0){
+            character = fgetc(ifp);
+        }
+    }
+
+    printf("\nToken List:\n");
+    
+    //Takes care of variable names
+    printf("%d", lex_list[0].token);
+    if(lex_list[0].token == 2){
+        printf(" %s", lex_list[0].name);
+    }
+    //Takes care of numbers
+    else if(lex_list[0].token == 3){
+        printf(" %d", lex_list[0].number_v);
+    }
+    //prints out variable names in Lexeme List
+    for(i = 1; i < lex_index; i++){
+        printf(" %d", lex_list[i].token);
+        if(lex_list[i].token == 2)
+            printf(" %s", lex_list[i].name);
+        else if(lex_list[i].token == 3)
+            printf(" %d", lex_list[i].number_v);
+    }
+    fclose(ifp);
   return 0;
 }
